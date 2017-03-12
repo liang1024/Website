2fec8450d4797c3bb3ad4945a4a5144eb7e863e4
Administrator <chinalzge@gmail.com>
2017-03-12 17:38:29 +0800

    add ~27

--- music/views.py
+++ music/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Album
-from django.shortcuts import render
+from .models import Album,Song
+from django.shortcuts import render,get_object_or_404
 from django.http import Http404
 
 
@@ -11,9 +11,40 @@ def index(request):
     return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
-def detail(request, albun_id):
-    try:
-        album = Album.objects.get(pk=albun_id)
-    except Album.DoesNotExist:  #id不存在异常
-        raise Http404("Album dies now exist")
+def detail(request, album_id):
+    album=get_object_or_404(Album,pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song=album.song_set.get(pk=request.POST['song'])
+    except(KeyError,Song.DoesNotExist):
+        return render(request,'music/detail.html',
+                      {'album':album,
+                    'error_message':'You did not select a valid song'})
+    else:
+        selected_song.is_favorite=True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
