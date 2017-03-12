b4f64ece2fde3694ef62833d4a76cd3a578e15cb
Administrator <chinalzge@gmail.com>
2017-03-12 11:57:10 +0800

    add ~16

--- music/views.py
+++ music/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import  HttpResponse
+from .models import Album
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>This is the Music app HomePage</h1>")
+    all_albums=Album.objects.all()
+    context={'all_albums':all_albums}
+    return render(request,'music/index.html',context)
 
 def detail(request,albun_id):
     return HttpResponse("<h2>Details for Album id :"+str(albun_id)+"</h2>")
